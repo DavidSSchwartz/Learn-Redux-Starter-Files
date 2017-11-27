25210428420678f039308dc1718971d0acb3d117
David S Schwartz <schwartzdavid2@gmail.com>
2017-11-27 15:44:58 +0200

    first

---
+++ learn-redux/client/components/Main.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+ const Main = React.createClass({
+	render(){	
+		return(
+			<div>
+				<h1>
+					<Link to="/">Reduxstagram></Link>
+				</h1>
+				{React.cloneElement(this.props.chidren, this.props)}
+			</div>
+			)
+		}
+	});
+
+
+export default Main;
+
+
+
+
+import React from 'react';
+import { Link } from 'react-router';
+
+const Main = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>
+          <Link to="/">Reduxstagram</Link>
+        </h1>
+        {React.cloneElement(this.props.children, this.props)}
+      </div>
+    )
+  }
+});
+
+export default Main;
